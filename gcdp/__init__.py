d543b2c13f981e3ff1effe92484b5d233aa44dc9
HalvardBariller <h.bariller@hotmail.fr>
2024-06-28 17:36:02 +0200

    Cleaned project

---
+++ gcdp/__init__.py
@@ -0,0 +1,6 @@
+"""
+The gcdp package contains modules for diffusion processes, episodes, and utilities.
+
+This package provides various functionalities to handle and simulate diffusion processes,
+manage episodes, and perform utility operations.
+"""
