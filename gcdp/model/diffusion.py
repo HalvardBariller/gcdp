96d3bf9cdd9f153763af80c23b43f5e80f15feb8
HalvardBariller <bariller@gpu.scai.sorbonne-universite.fr>
2024-08-29 16:03:55 +0200

    Improved vision encoder

--- gcdp/model/diffusion.py
+++ gcdp/model/diffusion.py
@@ -17,11 +17,14 @@
 # `cond` is applied to `x` with [FiLM](https://arxiv.org/abs/1709.07871) conditioning.
 
 
+import numpy as np
+from omegaconf import DictConfig
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 import torchvision
 import math
-from typing import Union, Callable
+from typing import Tuple, Union, Callable
 
 
 class SinusoidalPosEmb(nn.Module):
@@ -452,3 +455,242 @@ def replace_bn_with_gn(
         ),
     )
     return root_module
+
+
+class DiffusionRgbEncoder(nn.Module):
+    """Encoder an RGB image into a 1D feature vector.
+
+    Includes the ability to normalize and crop the image first.
+    """
+
+    def __init__(
+        self,
+        cfg: DictConfig,
+        #  weights: str | None = None,
+    ):
+        """Initialize the vision encoder with configuration."""
+        super().__init__()
+        self.vision_backbone = cfg.model.vision_encoder.name
+        self.pretrained_backbone_weights = cfg.model.vision_encoder.weights
+        crop_shape = cfg.model.vision_encoder.crop_shape
+        crop_is_random = cfg.model.vision_encoder.crop_is_random
+        use_group_norm = cfg.model.vision_encoder.use_group_norm
+        spatial_softmax_num_keypoints = (
+            cfg.model.vision_encoder.spatial_softmax_num_keypoints
+        )
+        # Set up optional preprocessing.
+        if crop_shape is not None:
+            self.do_crop = True
+            # Always use center crop for eval
+            self.center_crop = torchvision.transforms.CenterCrop(crop_shape)
+            if crop_is_random:
+                self.maybe_random_crop = torchvision.transforms.RandomCrop(
+                    crop_shape
+                )
+            else:
+                self.maybe_random_crop = self.center_crop
+        else:
+            self.do_crop = False
+
+        # Set up backbone.
+        # backbone_model = getattr(torchvision.models, self.vision_backbone)(
+        #     # weights=self.pretrained_backbone_weights
+        #     weights=weights
+        # )
+        func = getattr(torchvision.models, cfg.model.vision_encoder.name)
+        backbone_model = func(weights=self.pretrained_backbone_weights)
+        # Note: This assumes that the layer4 feature map is children()[-3]
+        self.backbone = nn.Sequential(*(list(backbone_model.children())[:-2]))
+        if use_group_norm:
+            if self.pretrained_backbone_weights:
+                raise ValueError(
+                    "You can't replace BatchNorm in a pretrained model without ruining the weights!"
+                )
+            self.backbone = _replace_submodules(
+                root_module=self.backbone,
+                predicate=lambda x: isinstance(x, nn.BatchNorm2d),
+                func=lambda x: nn.GroupNorm(
+                    num_groups=x.num_features // 16,
+                    num_channels=x.num_features,
+                ),
+            )
+
+        # Set up pooling and final layers.
+        # Use a dry run to get the feature map shape.
+        # The dummy input should take the number of image channels from `config.input_shapes` and it should
+        # use the height and width from `config.crop_shape` if it is provided, otherwise it should use the
+        # height and width from `config.input_shapes`.
+        image_keys = [
+            k
+            for k in cfg.model.input_shapes
+            if k.startswith("observation.image")
+        ]
+        # Note: we have a check in the config class to make sure all images have the same shape.
+        image_key = image_keys[0]
+        dummy_input_h_w = (
+            crop_shape
+            if crop_shape is not None
+            else cfg.input_shapes[image_key][1:]
+        )
+        dummy_input = torch.zeros(
+            size=(1, cfg.model.input_shapes[image_key][0], *dummy_input_h_w)
+        )
+        with torch.inference_mode():
+            dummy_feature_map = self.backbone(dummy_input)
+        feature_map_shape = tuple(dummy_feature_map.shape[1:])
+        self.pool = SpatialSoftmax(
+            feature_map_shape, num_kp=spatial_softmax_num_keypoints
+        )
+        self.feature_dim = spatial_softmax_num_keypoints * 2
+        self.out = nn.Linear(
+            spatial_softmax_num_keypoints * 2, self.feature_dim
+        )
+        self.relu = nn.ReLU()
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """
+        Forward pass of the vision encoder.
+
+        Parameters:
+            x: (B, C, H, W) image tensor with pixel values in [0, 1].
+        Returns:
+            (B, D) image feature.
+        """
+        # Preprocess: maybe crop (if it was set up in the __init__).
+        if self.do_crop:
+            if self.training:  # noqa: SIM108
+                x = self.maybe_random_crop(x)
+            else:
+                # Always use center crop for eval.
+                x = self.center_crop(x)
+        # Extract backbone feature.
+        x = torch.flatten(self.pool(self.backbone(x)), start_dim=1)
+        # Final linear layer with non-linearity.
+        x = self.relu(self.out(x))
+        return x
+
+
+def _replace_submodules(
+    root_module: nn.Module,
+    predicate: Callable[[nn.Module], bool],
+    func: Callable[[nn.Module], nn.Module],
+) -> nn.Module:
+    """
+    Replace all submodules selected by the predicate with the output of func.
+
+    Args:
+        root_module: The module for which the submodules need to be replaced
+        predicate: Takes a module as an argument and must return True if the that module is to be replaced.
+        func: Takes a module as an argument and returns a new module to replace it with.
+    Returns:
+        The root module with its submodules replaced.
+    """
+    if predicate(root_module):
+        return func(root_module)
+
+    replace_list = [
+        k.split(".")
+        for k, m in root_module.named_modules(remove_duplicate=True)
+        if predicate(m)
+    ]
+    for *parents, k in replace_list:
+        parent_module = root_module
+        if len(parents) > 0:
+            parent_module = root_module.get_submodule(".".join(parents))
+        if isinstance(parent_module, nn.Sequential):
+            src_module = parent_module[int(k)]
+        else:
+            src_module = getattr(parent_module, k)
+        tgt_module = func(src_module)
+        if isinstance(parent_module, nn.Sequential):
+            parent_module[int(k)] = tgt_module
+        else:
+            setattr(parent_module, k, tgt_module)
+    # verify that all BN are replaced
+    assert not any(
+        predicate(m)
+        for _, m in root_module.named_modules(remove_duplicate=True)
+    )
+    return root_module
+
+
+class SpatialSoftmax(nn.Module):
+    """
+    Spatial Soft Argmax operation described in "Deep Spatial Autoencoders for Visuomotor Learning" by Finn et al (https://arxiv.org/pdf/1509.06113).
+
+    A minimal port of the robomimic implementation.
+    At a high level, this takes 2D feature maps (from a convnet/ViT) and returns the "center of mass"
+    of activations of each channel, i.e., keypoints in the image space for the policy to focus on.
+
+    Example: take feature maps of size (512x10x12). We generate a grid of normalized coordinates (10x12x2):
+    -----------------------------------------------------
+    | (-1., -1.)   | (-0.82, -1.)   | ... | (1., -1.)   |
+    | (-1., -0.78) | (-0.82, -0.78) | ... | (1., -0.78) |
+    | ...          | ...            | ... | ...         |
+    | (-1., 1.)    | (-0.82, 1.)    | ... | (1., 1.)    |
+    -----------------------------------------------------
+    This is achieved by applying channel-wise softmax over the activations (512x120) and computing the dot
+    product with the coordinates (120x2) to get expected points of maximal activation (512x2).
+
+    The example above results in 512 keypoints (corresponding to the 512 input channels). We can optionally
+    provide num_kp != None to control the number of keypoints. This is achieved by a first applying a learnable
+    linear mapping (in_channels, H, W) -> (num_kp, H, W).
+    """
+
+    def __init__(self, input_shape, num_kp=None):
+        """
+        Initialize the spatial softmax.
+
+        Args:
+            input_shape (list): (C, H, W) input feature map shape.
+            num_kp (int): number of keypoints in output. If None, output will have the same number of channels as input.
+        """
+        super().__init__()
+
+        assert len(input_shape) == 3
+        self._in_c, self._in_h, self._in_w = input_shape
+
+        if num_kp is not None:
+            self.nets = torch.nn.Conv2d(self._in_c, num_kp, kernel_size=1)
+            self._out_c = num_kp
+        else:
+            self.nets = None
+            self._out_c = self._in_c
+
+        # we could use torch.linspace directly but that seems to behave slightly differently than numpy
+        # and causes a small degradation in pc_success of pre-trained models.
+        pos_x, pos_y = np.meshgrid(
+            np.linspace(-1.0, 1.0, self._in_w),
+            np.linspace(-1.0, 1.0, self._in_h),
+        )
+        pos_x = torch.from_numpy(
+            pos_x.reshape(self._in_h * self._in_w, 1)
+        ).float()
+        pos_y = torch.from_numpy(
+            pos_y.reshape(self._in_h * self._in_w, 1)
+        ).float()
+        # register as buffer so it's moved to the correct device.
+        self.register_buffer("pos_grid", torch.cat([pos_x, pos_y], dim=1))
+
+    def forward(self, features: torch.Tensor) -> torch.Tensor:
+        """
+        Forward pass of the spatial softmax.
+
+        Args:
+            features: (B, C, H, W) input feature maps.
+        Returns:
+            (B, K, 2) image-space coordinates of keypoints.
+        """
+        if self.nets is not None:
+            features = self.nets(features)
+
+        # [B, K, H, W] -> [B * K, H * W] where K is number of keypoints
+        features = features.reshape(-1, self._in_h * self._in_w)
+        # 2d softmax normalization
+        attention = F.softmax(features, dim=-1)
+        # [B * K, H * W] x [H * W, 2] -> [B * K, 2] for spatial coordinate mean in x and y dimensions
+        expected_xy = attention @ self.pos_grid
+        # reshape to [B, K, 2]
+        feature_keypoints = expected_xy.view(-1, self._out_c, 2)
+
+        return feature_keypoints
